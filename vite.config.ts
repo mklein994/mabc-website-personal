0578a368c10fe561f8b12a6177a911fe5afb2196
Matthew Klein <mklein994@gmail.com>
2026-05-15 21:50:21 -0500

    Add @/* as an alias for ./src/*

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
+import { fileURLToPath } from "node:url";
+
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 
+const resolve = (path: string) => fileURLToPath(new URL(path, import.meta.url));
+
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": resolve("./src"),
+    },
+  },
+
   plugins: [vue(), tailwindcss()],
 });
