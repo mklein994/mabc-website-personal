989eb33c9d203c25b565b27ed1c3b677559a68db
Matthew Klein <mklein994@gmail.com>
2026-05-16 18:34:20 -0500

    Add "DM Sans" and "Merriweather" fonts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 
+import { vitePluginGoogleFonts } from "./config/vite-plugin-google-fonts";
+
 const resolve = (path: string) => fileURLToPath(new URL(path, import.meta.url));
 
 // https://vite.dev/config/
@@ -18,5 +20,31 @@ export default defineConfig({
     },
   },
 
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    vitePluginGoogleFonts({
+      fonts: [
+        {
+          family: "DM Sans",
+          specs: [0, 1].map((ital) => ({
+            ital,
+            opsz: "9..40",
+            wght: "100..1000",
+          })),
+        },
+
+        {
+          family: "Merriweather",
+          specs: [0, 1].map((ital) => ({
+            ital,
+            opsz: "18..144",
+            wght: "300..900",
+          })),
+        },
+      ],
+
+      display: "swap",
+    }),
+  ],
 });
