395393e6f93fb29b236d9125b787309cba49335e
Matthew Klein <mklein994@gmail.com>
2026-05-16 21:48:24 -0500

    Explain the lucide module override

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,6 +4,8 @@ declare module "*.vue" {
   export default component;
 }
 
+// https://lucide.dev/guide/vue/advanced/aliased-names
 declare module "@lucide/vue" {
+  // Limit Lucide icon imports to only those ending in `*Icon.vue`
   export * from "@lucide/vue/dist/lucide-vue.suffixed";
 }
