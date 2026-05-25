2a7d7501534fd3722dc3605f341d1a5ccd425340
Matthew Klein <mklein994@gmail.com>
2026-05-24 19:39:39 -0500

    Configure Vite to use a proxy for R2 assets when configured, or serve from public/r2/

--- vite.config.ts
+++ vite.config.ts
@@ -4,55 +4,73 @@ import { fileURLToPath } from "node:url";
 
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 import { vitePluginGoogleFonts } from "./config/vite-plugin-google-fonts";
 
 const resolve = (path: string) => fileURLToPath(new URL(path, import.meta.url));
 
 // https://vite.dev/config/
-export default defineConfig({
-  define: {
-    __VUE_OPTIONS_API__: false,
-    "import.meta.vitest": "undefined",
-  },
+export default defineConfig(({ mode }) => {
+  const cwd = resolve(".");
+  const env = loadEnv(mode, cwd, "BUILDTIME_");
 
-  resolve: {
-    alias: {
-      "@": resolve("./src"),
+  return {
+    define: {
+      __VUE_OPTIONS_API__: false,
+      "import.meta.vitest": "undefined",
     },
-  },
 
-  test: {
-    environment: "happy-dom",
-    includeSource: ["./src/**/*.ts", "./config/**/*.ts"],
-  },
+    server: {
+      proxy:
+        env.BUILDTIME_R2_LOCAL === "true"
+          ? undefined
+          : {
+              "/r2": {
+                target: env.BUILDTIME_ASSET_URL,
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/r2/, ""),
+              },
+            },
+    },
 
-  plugins: [
-    vue(),
-    tailwindcss(),
-    vitePluginGoogleFonts({
-      fonts: [
-        {
-          family: "DM Sans",
-          specs: [0, 1].map((ital) => ({
-            ital,
-            opsz: "9..40",
-            wght: "100..1000",
-          })),
-        },
+    resolve: {
+      alias: {
+        "@": resolve("./src"),
+      },
+    },
 
-        {
-          family: "Merriweather",
-          specs: [0, 1].map((ital) => ({
-            ital,
-            opsz: "18..144",
-            wght: "300..900",
-          })),
-        },
-      ],
+    test: {
+      environment: "happy-dom",
+      includeSource: ["./src/**/*.ts", "./config/**/*.ts"],
+    },
 
-      display: "swap",
-    }),
-  ],
+    plugins: [
+      vue(),
+      tailwindcss(),
+      vitePluginGoogleFonts({
+        fonts: [
+          {
+            family: "DM Sans",
+            specs: [0, 1].map((ital) => ({
+              ital,
+              opsz: "9..40",
+              wght: "100..1000",
+            })),
+          },
+
+          {
+            family: "Merriweather",
+            specs: [0, 1].map((ital) => ({
+              ital,
+              opsz: "18..144",
+              wght: "300..900",
+            })),
+          },
+        ],
+
+        display: "swap",
+      }),
+    ],
+  };
 });
